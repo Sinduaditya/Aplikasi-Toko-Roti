ee49952ea5e8c96a6773ddb90dc7dcaac9715272
SinduAditya <nduujanadi51@gmail.com>
2022-04-24 19:59:38 +0700

    first

---
+++ src/aplikasi/toko/roti/AplikasiTokoRoti.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplikasi.toko.roti;
+
+/**
+ *
+ * @author Asus
+ */
+public class AplikasiTokoRoti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+    }
+   public int hargajual; //ini variable untuk"
+   //menyimpan data
+   //harga jual
+   
+}
